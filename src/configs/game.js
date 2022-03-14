1feb5cc23232ff827c5f744290ac7baaaf32b6fc
Patrick Boutin <patrick.boutin@roberthalf.com>
2022-03-14 07:04:31 -0700

    Add new icon images and initial configuration settings.

---
+++ src/configs/game.js
@@ -0,0 +1,12 @@
+const settings = {
+  gameName: "Rock, paper, scissors",
+  userName: "Player 1",
+  pcName: "Computer",
+  winMessage: "You won!",
+  tieMessage: "It's a tie!",
+  lostMessage: "You lost!",
+  waitingMessage: "Waiting for selection",
+  winTarget: 10,
+};
+
+export settings;
